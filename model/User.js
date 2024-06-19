e8fefef4d54cf52d52afe86346bf78fd7acfa38b
vedivero <vedivero@github.com>
2024-06-19 11:42:41 +0900

    [Feat]User 스키마 컬럼 추가

--- model/User.js
+++ model/User.js
@@ -1,4 +1,4 @@
-const mongoose =require('mongoose')
+const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const jwt = require('jsonwebtoken')
 require('dotenv').config()
@@ -6,17 +6,24 @@ const secretKey = process.env.JWT_SECRET_KEY
 
 
 const userSchema = Schema({
-	email:{type:String, required:true, unique:true},
-	password:{type:String, required:true},
-	name:{type:String, required:true},
-	memo:{type:String, default:''},
-	image:{type:String, default:''},
-	level:{type:String, default:'customer'}, // customer, admin
-	credit:{type:Number, default:10000}, //가입과 동시에 만원 넣어줌
-	coupon:{type:Number, default:0}
-},{timestamps:true})
+	email: { type: String, required: true, unique: true },
+	password: { type: String, required: true },
+	name: { type: String, required: true },
+	memo: { type: String, default: '' },
+	image: { type: String, default: '' },
+	level: { type: String, default: 'customer' }, // customer, admin
+	credit: { type: Number, default: 10000 }, //가입과 동시에 만원 넣어줌
+	coupon: { type: Number, default: 0 },
+	shipTo: { type: Array, default: [] },
+	totalPurchase: { type: Number, default: 0 },
+	wishList: { type: Array, default: [] },
+	orders: { type: Array, default: [] },
+	purchasedItems: { type: Object, default: {} },
+	viewed: { type: Array, default: [] },
+	reviewIds: { type: Array, default: [] }
+}, { timestamps: true })
 
-userSchema.methods.toJSON =function(){
+userSchema.methods.toJSON = function () {
 	const obj = this._doc
 	delete obj.password
 	delete obj.__v
@@ -25,8 +32,8 @@ userSchema.methods.toJSON =function(){
 	return obj
 }
 
-userSchema.methods.generateToken = async function(){
-	const token = await jwt.sign({_id:this._id}, secretKey,{expiresIn:"1d"})
+userSchema.methods.generateToken = async function () {
+	const token = await jwt.sign({ _id: this._id }, secretKey, { expiresIn: "1d" })
 	return token;
 }
 
